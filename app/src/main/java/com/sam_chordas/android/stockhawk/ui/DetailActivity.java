dc86a8527c9eb113ecea23a46f52172ec9390894
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2016-07-13 12:40:50 +0530

    Added code for sharing image and text via Intent

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.sam_chordas.android.stockhawk.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -29,6 +33,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
@@ -196,7 +203,37 @@ public class DetailActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             Toast.makeText(DetailActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            // save bitmap to cache directory
+            try {
 
+                File cachePath = new File(getBaseContext().getCacheDir(), "images");
+                cachePath.mkdirs(); // don't forget to make the directory
+                FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+                lineChart.getChartBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream);
+                stream.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Share the Image
+            File imagePath = new File(getBaseContext().getCacheDir(), "images");
+            File newFile = new File(imagePath, "image.png");
+            Uri contentUri = FileProvider.getUriForFile(getBaseContext(), "com.sam_chordas.android.stockhawk.fileprovider", newFile);
+
+            if (contentUri != null) {
+
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+                shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
+                shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Stock Price for " + name);
+                startActivity(Intent.createChooser(shareIntent, "Choose an app"));
+
+            }
         }
     };
 }
